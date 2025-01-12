5cd2fd81e4d73c36d3eebbb2c55819019bc72622
TijnAltenaAO <64588832+TijnAltenaAO@users.noreply.github.com>
2025-01-12 14:00:38 +0100

    basic wireless comms between esp's

--- Receiver_master_code/Receiver_master_code.ino
+++ Receiver_master_code/Receiver_master_code.ino
@@ -1,143 +1,64 @@
-#include <ESP32Servo.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp-now-many-to-one-esp32/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+#include <esp_now.h>
+#include <WiFi.h>
 
-#define MPU9250_ADDR 0x68
-// Define screen dimensions
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-// Define I2C address for ssd1306
-#define OLED_ADDR 0x3C
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+  int id;
+  int x;
+}struct_message;
 
-// Create SSD1306 object
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+// Create a struct_message called myData
+struct_message myData;
 
-// Define servo object
-Servo myServo;
+// Create a structure to hold the readings from each board
+struct_message board1;
+struct_message board2;
 
-int potPin = 34;
-int buttonPin = 27;
-int servoPin = 18;
-int potValue;
-int stepValue;
-int levelTime;
-volatile bool gamePaused = true;  // game paused or not.
+// Create an array with all the structures
+struct_message boardsStruct[2] = {board1, board2};
 
-// Interrupt Service Routine (ISR) for the button
-void IRAM_ATTR handleButtonPress() {
-  gamePaused = !gamePaused;  // Toggle game paused state
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
+  // Update the structures with the new incoming data
+  boardsStruct[myData.id-1].x = myData.x;
+  Serial.printf("x value: %d \n", boardsStruct[myData.id-1].x);
+  Serial.println();
 }
-
+ 
 void setup() {
-  // Initialize Serial Monitor
+  //Initialize Serial Monitor
   Serial.begin(115200);
-  delay(1000);
+  delay(500);
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
 
-  // Initialize UART1
-  Serial2.begin(9600, SERIAL_8N1, 16, 17);  // Replace RX1_PIN and TX1_PIN with your GPIOs
-
-  // Initialize UART2
-  Serial1.begin(9600, SERIAL_8N1, 25, 26);  // RX2_PIN = GPIO 25, TX2_PIN = GPIO 26
-
-  pinMode(potPin, INPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
-
-  // Attach interrupt to the button pin
-  attachInterrupt(digitalPinToInterrupt(buttonPin), handleButtonPress, FALLING);
-
-  myServo.attach(servoPin, 500, 2500);  // 500 µs to 2500 µs corresponds to 0° to 180° (figure out if not limited to 120 or 160)
-
-  // Initialize display
-  if (!display.begin(SSD1306_PAGEADDR, OLED_ADDR)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ;
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
   }
-
-  // Clear display buffer
-  display.clearDisplay();
-
-  // Set text size and color
-  display.setTextSize(3);  // Normal size
-  display.setTextColor(SSD1306_WHITE);
-
-  // Display text
-  display.setCursor(0, 10);
-  display.display();
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 }
-
-String decideWinner(int base, int val, int val1) {
-  int diff = abs(val - base);
-  int diff1 = abs(val1 - base);
-
-  // Check if < suffices. maybe implement = case?
-  return (diff < diff1) ? "p1" : "p2";
-}
-
+ 
 void loop() {
-  if (gamePaused) {
-    // read potpin to gather level.
-    potValue = analogRead(potPin);  // Read the potentiometer value (0-4095)
-
-    // Map the raw value to 4 discrete steps: 0, 1, 2, 3
-    stepValue = map(potValue, 0, 4095, 1, 3);
-    display.clearDisplay();
-    display.setCursor(0, 10);
-    display.println(stepValue);
-    display.display();
-
-  } else {
-    for (int i = 0; i < 3; i++) {
-      // send level to slaves, and generate and send angle.
-      randomSeed(esp_random());  // Seed the random generator with hardware RNG
-      int randomValue = random(0, 161);
-
-      switch (stepValue) {
-        case 1:
-          levelTime = 5000;
-          break;
-        case 2:
-          levelTime = 10000;
-          break;
-        case 3:
-          levelTime = 15000;
-          break;
-      }
-
-      Serial1.print(levelTime);
-      Serial2.print(levelTime);
-
-      // also write angle to master servo.
-      myServo.write(randomValue);
-      display.clearDisplay();
-      display.setCursor(0, 10);
-      display.println(randomValue);
-      display.display();
-
-      delay(levelTime);
-      // read angles from slaves.
-      while (Serial1.available() > 0) {
-        Serial1.read();
-      }
-      while (Serial2.available() > 0) {
-        Serial2.read();
-      }
-
-      int data1 = Serial1.readStringUntil('\n').toInt();
-      int data2 = Serial2.readStringUntil('\n').toInt();
-
-      Serial.println(data1);
-      Serial.println(data2);
-
-      // decide winner
-      display.clearDisplay();
-      display.setCursor(0, 10);
-      display.println("W = " + decideWinner(randomValue, data1, data2));
-      display.display();
-      delay(5000);
-
-      gamePaused = true;
-    }
-  }
+  // Acess the variables for each board
+  int board1X = boardsStruct[0].x;
+  int board2X = boardsStruct[1].x;
 }
--- transmitter_slave_code/transmitter_slave_code.ino
+++ transmitter_slave_code/transmitter_slave_code.ino
@@ -1,107 +1,77 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp-now-many-to-one-esp32/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+#include <esp_now.h>
+#include <WiFi.h>
 
-const int MPU_addr = 0x68;
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+// REPLACE WITH THE RECEIVER'S MAC Address
+uint8_t broadcastAddress[] = {0xd8, 0xbc, 0x38, 0xfd, 0x5a, 0xcc};
 
-int minVal = 265;
-int maxVal = 402;
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+    int id; // must be unique for each sender board
+    int x;
+    int y;
+} struct_message;
 
-double x;
+// Create a struct_message called myData
+struct_message myData;
 
-// Define screen dimensions
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-// Define I2C address for ssd1306
-#define OLED_ADDR 0x3C
+// Create peer interface
+esp_now_peer_info_t peerInfo;
 
-// Create SSD1306 object
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
-#include <ESP32Servo.h>
-
-// Define servo object
-Servo myServo;
-
-// Define GPIO pin for the servo
-#define SERVO_PIN 18
-
-
-int servo_angle;
-
-void setup() {
-  Serial.begin(9600);
-  myServo.attach(SERVO_PIN, 500, 2500);  // 500 µs to 2500 µs corresponds to 0° to 180°
-
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-
-  if (!display.begin(SSD1306_PAGEADDR, OLED_ADDR)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ;
-  }
-
-  // Clear display buffer
-  display.clearDisplay();
-
-  // Set text size and color
-  display.setTextSize(3);  // Normal size
-  display.setTextColor(SSD1306_WHITE);
-
-  // Display text
-  display.setCursor(0, 10);
-  display.println("Hello, ESP32!");
-  display.display();
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+  delay(500);
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
 
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
 void loop() {
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr, 14, true);
-  AcX = Wire.read() << 8 | Wire.read();
-  AcY = Wire.read() << 8 | Wire.read();
-  AcZ = Wire.read() << 8 | Wire.read();
-  int xAng = map(AcX, minVal, maxVal, -90, 90);
-  int yAng = map(AcY, minVal, maxVal, -90, 90);
-  int zAng = map(AcZ, minVal, maxVal, -90, 90);
-  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  // Set values to send
+  myData.id = 2;
+  myData.x = random(0,50);
 
-  // Serial.print("AngleX= ");
-
-  if (x > 90 && x < 270) {
-    int diffX90 = abs(x - 90);
-    int diffX270 = abs(x - 270);
-
-    if (diffX90 > diffX270) {
-      Serial.println(diffX90);
-      Serial.println(diffX270);
-      Serial.println(x);
-      x = 180;
-    } else {
-      // check if could be 0? weird behavior lol.
-      x = 90;
-    }
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
   }
 
-  if (x <= 90) {
-    x = map(x, 0, 90, 90, 0);
-  } else if (x > 270) {
-    x = map(x, 270, 360, 180, 90);
-  }
-
-  Serial.println(x);
-
-  display.clearDisplay();
-  display.setCursor(0, 10);
-  display.println(x);
-  display.display();
-
-  myServo.write(x);
-  delay(20);
 }
